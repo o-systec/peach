7093c3e25bb63aca41952208e435bea86460bea2
yanggz <guozheng-yang@systechn.com>
2020-02-25 11:33:00 +0800

    fix

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 docker network create icloud
 docker volume create peach
 docker rm -f peach
-docker run -d --name peach --network icloud -p 10086:22 -v hello:/home/admin/.ssh hello
+docker run -d --name peach --network icloud -p 10086:22 -v peach:/home/admin/.ssh peach
 
